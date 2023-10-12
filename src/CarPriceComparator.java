caeee1239fd9f5f881e5dec09a7c3246c4affedb
ShubhadaRaut05 <shubhadamali05@gmail.com>
2023-10-12 11:18:57 +0530

    added cars to list and toString

--- src/CarPriceComparator.java
+++ src/CarPriceComparator.java
@@ -5,4 +5,6 @@ public class CarPriceComparator implements Comparator<Car> {
     public int compare(Car o1, Car o2) {
         return o1.getPrice()-o2.getPrice();
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Car Class Demo");
+        Car c1=new Car(100000,1200);
+        Car c2=new Car(500000,1000);
+        Car c3=new Car(200000,800);
+        Car c4=new Car(400000,600);
+        List<Car> carList=new ArrayList<>(Arrays.asList(c1,c2,c3,c4));
+        carList.sort(new CarPriceComparator());
+        System.out.println(carList);
+
+
     }
 }
